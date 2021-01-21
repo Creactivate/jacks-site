a94121a3e707fefd123cf679a3db68b8efec440a
Luke Hurlock <luke@lukehurlock.co.uk>
2021-01-21 21:32:08 +0000

    enter page proto and signature added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {
   Switch,
   Route, Link, HashRouter,
 } from "react-router-dom";
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import LightBoxModal from './components/lightbox-modal';
@@ -14,12 +14,69 @@ import Container from 'react-bootstrap/Container';
 
 function App() {
   const [show, setShow] = useState({show:false, index:0});
+  const [firstScreen, setFirst] = useState(true);
+
+  useEffect(() => {
+
+  }, []);
 
   const onShowChange = (bool, index) => {
       setShow({show:bool,index:index});
   }
 
   const imageURLs = fileObject.images;
+
+  const flicker = async () => {
+    const img = document.querySelector('.firstScreenImage');
+    let blend = 'hue';
+    document.querySelector('.firstScreenClose').style.display = 'block';
+
+    
+
+    function toggleLights() {
+      //toggle hue to hard-light;
+      if (blend === 'hue') {
+        blend = 'hard-light';
+        img.style.mixBlendMode = 'hard-light'
+        console.log('hard-light');
+      } else {
+        blend = 'hue';
+        img.style.mixBlendMode = 'hue'
+        console.log('hue');
+      }
+    }
+    
+
+    
+    let toggle1 = setInterval(toggleLights, 300);
+    await setTimeout(()=>{
+      clearInterval(toggle1);
+      img.style.mixBlendMode = 'hue';
+      img.style.mixBlendMode = 'unset';
+      console.log('oh right');
+    }, 3000);
+    let toggle2 = setInterval(toggleLights, 100);
+    await setTimeout(()=>{
+      clearInterval(toggle2);
+      img.style.mixBlendMode = 'hue';
+      img.style.mixBlendMode = 'unset';
+    }, 600);
+    let toggle3 = setInterval(toggleLights, 50);
+    await setTimeout(()=>{
+      clearInterval(toggle3);
+      img.style.mixBlendMode = 'hue';
+      img.style.mixBlendMode = 'unset';
+    }, 600);
+    await setTimeout(()=>{
+      document.querySelector('.firstScreenLogo').style.display = 'block';
+    }, 4000);
+
+  
+    
+    
+      
+     
+  }
 
   return (
     <Router>
@@ -28,13 +85,12 @@ function App() {
           
 
             
-              <Link className='navbarBrand' to="/">Jack Poppleton Art</Link>
+              <Link className='navbarBrand' to="/"><img src="./images/jack-signature.png" alt="" /></Link>
             
 
             
-                <Link className='navLink' to="/home">About</Link>
-                <Link className='navLink' to="/features">Gallery</Link>
-                <Link className='navLink' to="/pricing">Contact</Link>
+                <button type="button" className='navLink'>About</button>
+                <button type="button" className='navLink'>Contact</button>
               
             
 
@@ -44,7 +100,8 @@ function App() {
 
         <Switch>
 
-          <Route path='/jacks-site' exact>
+          <Route path='/jacks-site/enter' exact>
+
             <div className='App-Body'>
               {imageURLs.map((image, index) => 
                 <>
@@ -69,6 +126,16 @@ function App() {
                 onShowChange={onShowChange}
               />
 
+          </Route>
+          <Route path="/jacks-site/">
+            <div className="firstScreen">
+              <div className="cont">
+                <img src="./images/jack-signature.png" className="firstScreenLogo fade-in" alt="" />
+                <img onLoad={flicker} className="firstScreenImage" src="./images/jack-poppleton-untitled-2.jpg" alt=""/>
+              </div>
+              
+              <a className="firstScreenClose" href="/jacks-site/enter">Enter Site</a>
+            </div>
           </Route>
 
         </Switch>
